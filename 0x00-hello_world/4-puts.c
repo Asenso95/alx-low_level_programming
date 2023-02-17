5391c754955b533fe7ca9e6b428023005ce7fb63
Benjamin Akwasi Asenso <asenso95@gmail.com>
2023-02-16 20:31:09 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio>
+
+
+/**
+ * main - prints "Programming is like building a multilingual
+ * puzzle, followed by a newline".
+ * return 0.
+ */
+
+int main(void)
+
+{
+	puts("\"Programmiing is like building a multilingual puzzel");
+
+	return(0);
+}
