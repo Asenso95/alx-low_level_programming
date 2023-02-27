c6e23facc89af8b9e05bd5068a4e10d2b8f96955
Benjamin Akwasi Asenso <asenso95@gmail.com>
2023-02-27 08:25:08 -0800

    My commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * print_alphabet_x10 - Prints the alphabet 10 times
+ * print_alphabet_x10 - Prints the alphabet 10 times.
  *
  * Return: Always 0 (Success)
  */
 void print_alphabet_x10(void)
 {
 	int i;
-	char j;
+	int j;
 
 	for (j = 1, i <= 10; i++)
 	{
