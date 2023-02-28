8acbbcdaffa5f5a7e55e46b0534a9df26212c9b2
Benjamin Akwasi Asenso <asenso95@gmail.com>
2023-02-27 18:01:55 -0800

    My commit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -4,14 +4,13 @@
  * print_last_digit - Prints the last digit
  * of a number.
  *
- * @n: input number as a integer.
+ * @n: input number as an integer.
  *
  * Return: last digit.
  */
-
 int print_last_digit(int n)
 {
-	int l,
+	int l;
 
 	l = n % 10;
 	if (l < 0)
